b31e7cee14dba8161e487fbc5147f105e0e96f1c
Enrico Fanizzi <enrico.fanizzi@refresh-academy.org>
2025-11-27 19:46:27 +0100

    correzione degli ids nelle dim_genere e dim_fascia_eta

--- scripts_sondaggio/dim_genere_dim_fascia_eta.sql
+++ scripts_sondaggio/dim_genere_dim_fascia_eta.sql
@@ -6,15 +6,18 @@ select pa.fascia_eta from progetto_andromeda pa
 drop table if exists dim_genere;
 
 create table dim_genere as 
-select distinct row_number() over () as ids_genere, pa.genere 
-from progetto_andromeda pa;
-
+select row_number() over () as ids_genere, * from (
+	select distinct pa.genere 
+	from progetto_andromeda pa
+);
 
 drop table if exists dim_fascia_eta;
 
 create table dim_fascia_eta as 
-select distinct row_number() over () as ids_fascia_eta, pa.fascia_eta
-from progetto_andromeda pa;
+ select row_number() over () as ids_fascia_eta, * from (
+   select distinct pa.fascia_eta
+from progetto_andromeda pa
+);
 
 select * from dim_fascia_eta dfe
 
