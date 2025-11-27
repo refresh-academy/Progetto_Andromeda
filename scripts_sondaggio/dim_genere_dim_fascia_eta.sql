e6a9c0992b65b1cb191d644b17e712ab676f5d12
Enrico Fanizzi <enrico.fanizzi@refresh-academy.org>
2025-11-27 17:23:51 +0100

    messi gli ids nelle dim_fascia_eta e dim_genere

--- scripts_sondaggio/dim_genere_dim_fascia_eta.sql
+++ scripts_sondaggio/dim_genere_dim_fascia_eta.sql
@@ -1,9 +1,4 @@
-select * from progetto_andromeda pa 
-
 select * from progetto_andromeda pa where pa.genere 
-
-drop table if exists dim_genere as
-create table dim_genere as select pa.genere from progetto_andromeda pa --non ha senso
 
 select pa.fascia_eta from progetto_andromeda pa 
 
@@ -11,18 +6,16 @@ select pa.fascia_eta from progetto_andromeda pa
 drop table if exists dim_genere;
 
 create table dim_genere as 
-select distinct pa.genere 
+select distinct row_number() over () as ids_genere, pa.genere 
 from progetto_andromeda pa;
 
 
 drop table if exists dim_fascia_eta;
 
 create table dim_fascia_eta as 
-select distinct pa.fascia_eta
+select distinct row_number() over () as ids_fascia_eta, pa.fascia_eta
 from progetto_andromeda pa;
 
 select * from dim_fascia_eta dfe
 
 select * from dim_genere dg 
-
-
