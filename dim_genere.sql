da23f157c5e92cf8e198ee38abc504d06e260e41
Enrico Fanizzi <enrico.fanizzi@refresh-academy.org>
2025-11-27 15:28:08 +0100

    qui ho messo le query che hanno reso disponibile la dim_genere e dim_fascia_eta

--- dim_genere.sql
+++ dim_genere.sql
@@ -1,3 +1,28 @@
 select * from progetto_andromeda pa 
 
+select * from progetto_andromeda pa where pa.genere 
+
+drop table if exists dim_genere as
+create table dim_genere as select pa.genere from progetto_andromeda pa --non ha senso
+
+select pa.fascia_eta from progetto_andromeda pa 
+
+
+drop table if exists dim_genere;
+
+create table dim_genere as 
+select distinct pa.genere 
+from progetto_andromeda pa;
+
+
+drop table if exists dim_fascia_eta;
+
+create table dim_fascia_eta as 
+select distinct pa.fascia_eta
+from progetto_andromeda pa;
+
+select * from dim_fascia_eta dfe
+
+select * from dim_genere dg 
+
 
