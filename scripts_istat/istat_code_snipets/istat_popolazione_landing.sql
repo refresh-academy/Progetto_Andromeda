dc3f55016cd51d09b1cf51d971c59bf8db19247c
goblin-code-squad <alessio.pedrotti@refresh-academy.org>
2025-12-10 16:15:37 +0100

    upload snipets code per istat

---
+++ scripts_istat/istat_code_snipets/istat_popolazione_landing.sql
@@ -0,0 +1,12 @@
+set search_path to istat_landing;
+
+
+SET SEARCH_PATH  to 
+DROP TABLE if exists istat_landing.popolazione_italia_regioni_province;
+
+CREATE TABLE istat_landing.lt_popolazione_italia_regioni_province (
+);
+
+DROP TABLE if exists lt_condanne_reati_violenti_sesso_reg;
+
+create table lt_condanne_reati_violenti
---
+++ scripts_istat/istat_code_snipets/istat_tabelle_landing_per_import.sql
@@ -0,0 +1,11 @@
+
+
+DROP TABLE if exists istat_landing.popolazione_italia_regioni_province;
+CREATE TABLE istat_landing.lt_popolazione_italia_regioni_province (
+);
+
+DROP TABLE if exists istat_landing.lt_condanne_reati_violenti_sesso_reg;
+
+CREATE TABLE istat_landing.lt_condanne_reati_violenti_sesso_reg (
+
+);
